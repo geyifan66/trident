208e3811972f9162742fe5e0b468f1ab1e9d09d0
zhiqiang.ge <zhiqiang.ge@dmall.com>
2019-09-07 23:03:08 +0800

    intit

---
+++ blackpearl/src/main/java/com/ewan/blackpearl/BlackpearlApplication.java
@@ -0,0 +1,13 @@
+package com.ewan.blackpearl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackpearlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackpearlApplication.class, args);
+    }
+
+}
